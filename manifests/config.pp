e51001d8420dc18ed7665a0f83edd423db879c6c
Maksym Melnychok <maksym@yelp.com>
2015-08-19 14:01:09 +0200

    make config class ensure-aware

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,17 +3,21 @@
 # This class is called from redis_sentinel
 #
 class redis_sentinel::config {
+  if $redis_sentinel::ensure == 'present' {
+    concat { $redis_sentinel::config_file:
+      owner => $redis_sentinel::user,
+      group => $redis_sentinel::group,
+      mode  => '0440'
+    }
 
-  concat { $redis_sentinel::config_file:
-    owner => $redis_sentinel::user,
-    group => $redis_sentinel::group,
-    mode  => '0440',  
+    concat::fragment { 'sentinel_header':
+      target  => $redis_sentinel::config_file,
+      content => "#This file managed by the redis_sentinel puppet module\ndaemonize yes\nlogfile /var/log/redis/redis-sentinel.log\npidfile /var/run/redis/redis-sentinel.pid",
+      order   => '01',
+    }
+  } else {
+    file { $redis_sentinel::config_file:
+      ensure => absent
+    }
   }
-  
-  concat::fragment { 'sentinel_header': 
-    target  => $redis_sentinel::config_file,
-    content => "#This file managed by the redis_sentinel puppet module\ndaemonize yes\nlogfile /var/log/redis/redis-sentinel.log\npidfile /var/run/redis/redis-sentinel.pid",
-    order   => '01',
-  }
-
 }
