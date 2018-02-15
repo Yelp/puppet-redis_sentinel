9b3fff00ae8c13a989b0fa0fb7c112f09ace5b6d
Paul O'Connor <465717+pauloconnor@users.noreply.github.com>
2018-02-15 15:37:22 +0000

    Add systemd service

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,33 +1,44 @@
-# == Class redis_sentinel::intall
+# == Class redis_sentinel::isntall
 #
 class redis_sentinel::install {
 
-  #TODO: This isn't really a good way to figure out if an OS is upstart capable or not
-  case $::osfamily {
-    'RedHat': {
-      file { '/etc/init.d/redis-sentinel':
-        ensure   => $redis_sentinel::ensure,
-        mode     => '0555',
-        owner    => 'root',
-        group    => 'root',
-        content => template('redis_sentinel/init.erb'),
-      }
-    }
-    'Debian': {
-      file { '/etc/init/redis-sentinel.conf':
-        ensure   => $redis_sentinel::ensure,
-        mode     => '0444',
-        owner    => 'root',
-        group    => 'root',
-        content => template('redis_sentinel/upstart.erb'),
-      }
-      file { '/etc/init.d/redis-sentinel':
-       ensure => 'link',
-       target => '/lib/init/upstart-job',
-       force  => true,
-      }
+  if $facts['systemd'] {
+    file { '/etc/systemd/system/redis-sentinel.service':
+      ensure   => $redis_sentinel::ensure,
+      mode     => '0555',
+      owner    => 'root',
+      group    => 'root',
+      content => template('redis_sentinel/systemd.erb'),
 
     }
-    default: { fail('Unknown OS') }
-  } 
+  } else {
+    #TODO: This isn't really a good way to figure out if an OS is upstart capable or not
+    case $::osfamily {
+      'RedHat': {
+        file { '/etc/init.d/redis-sentinel':
+          ensure   => $redis_sentinel::ensure,
+          mode     => '0555',
+          owner    => 'root',
+          group    => 'root',
+          content => template('redis_sentinel/init.erb'),
+        }
+      }
+      'Debian': {
+        file { '/etc/init/redis-sentinel.conf':
+          ensure   => $redis_sentinel::ensure,
+          mode     => '0444',
+          owner    => 'root',
+          group    => 'root',
+          content => template('redis_sentinel/upstart.erb'),
+        }
+        file { '/etc/init.d/redis-sentinel':
+         ensure => 'link',
+         target => '/lib/init/upstart-job',
+         force  => true,
+        }
+
+      }
+      default: { fail('Unknown OS') }
+    }
+  }
 }
